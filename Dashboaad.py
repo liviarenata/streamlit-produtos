d5dfe555f4ea83dc0cfa6b5e81c299c7622e0a38
Lívia Renata <74880716+liviarenata@users.noreply.github.com>
2024-07-04 16:38:43 -0300

    Update Dashboaad.py

--- Dashboaad.py
+++ Dashboaad.py
@@ -42,7 +42,7 @@ if filtro_vendedores:
 receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
 receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
 
-receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='ME'))['Preço'].sum().reset_index()
 receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
 receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
 
